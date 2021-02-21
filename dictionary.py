441425b7f8410dedd74db3e28d3fc7d8dab6fea8
maninserg <manin.serg@gmail.com>
2021-02-21 22:50:15 +0300

    Refactoring func get description

--- dictionary.py
+++ dictionary.py
@@ -2,18 +2,23 @@ import json
 from difflib import get_close_matches
 from phonemizer import phonemize
 
-data = json.load(open("dictionary.json"))
+
+
+def get_phonetic(w):
+        return phonemize(w,backend="espeak")
 
 
 def get_description(w):
+    data = json.load(open("dictionary.json"))
     w = w.lower()
     if w in data:
-        return data[w]
+        return (get_phonetic(w), data[w])
     elif len(get_close_matches(w, data.keys())) > 0:
         yn = input("Did you mean %s instead? Enter Y if yes, or N if no: " %
                    get_close_matches(w, data.keys())[0])
         if yn == "Y":
-            return data[get_close_matches(w, data.keys())[0]]
+            return (get_phonetic(get_close_matches(w, data.keys())[0],
+                                 data[get_close_matches(w, data.keys())[0]]))
         elif yn == "N":
             return "The word doesn't exist. Please double check it"
         else:
@@ -21,15 +26,11 @@ def get_description(w):
     else:
         return "The word doesn't exist. Please double check it"
 
-
-def get_phonetic(w):
-        return phonemize(w,backend="espeak")
-
-
 def main():
     word = input("Enter word: ")
-    print(get_phonetic(word))
-    print(get_description(word))
+    print(get_description(word)[0])
+    print(get_description(word)[1])
 
 if __name__ == "__main__":
-    main()
+    while True:
+        main()
