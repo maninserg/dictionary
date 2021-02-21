d1413da64b3a3350bdd9aaef45615cffcead501b
maninserg <manin.serg@gmail.com>
2021-02-21 23:41:13 +0300

    Add func for pretty print results

--- dictionary.py
+++ dictionary.py
@@ -1,6 +1,7 @@
 import json
 from difflib import get_close_matches
 from phonemizer import phonemize
+import shutil
 
 
 
@@ -12,13 +13,14 @@ def get_description(w):
     data = json.load(open("dictionary.json"))
     w = w.lower()
     if w in data:
-        return (get_phonetic(w), data[w])
+        return (w, get_phonetic(w), data[w])
     elif len(get_close_matches(w, data.keys())) > 0:
         yn = input("Did you mean %s instead? Enter Y if yes, or N if no: " %
                    get_close_matches(w, data.keys())[0])
         if yn == "Y":
-            return (get_phonetic(get_close_matches(w, data.keys())[0],
-                                 data[get_close_matches(w, data.keys())[0]]))
+            return (get_close_matches(w, data.keys())[0],
+                    get_phonetic(get_close_matches(w, data.keys())[0]),
+                    data[get_close_matches(w, data.keys())[0]])
         elif yn == "N":
             return "The word doesn't exist. Please double check it"
         else:
@@ -26,10 +28,28 @@ def get_description(w):
     else:
         return "The word doesn't exist. Please double check it"
 
+def pretty_print_description(w, ph, descrip):
+    width = shutil.get_terminal_size().columns
+    title = "{} - /{}/".format(w, ph)
+    border = "=" * len(title)
+    print(border.center(width))
+    print(title.center(width))
+    print(border.center(width))
+    print("")
+    print(descrip)
+    print("")
+    print("+" * width)
+    print("")
+
+
 def main():
     word = input("Enter word: ")
-    print(get_description(word)[0])
-    print(get_description(word)[1])
+    tp = get_description(word)
+    if  len(tp) == 3:
+        pretty_print_description(tp[0], tp[1], tp[2])
+    else:
+        print(tp)
+
 
 if __name__ == "__main__":
     while True:
