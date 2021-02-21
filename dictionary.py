ed43def10e97d4dc42dd975769b17b0ef3ae96be
maninserg <manin.serg@gmail.com>
2021-02-21 17:37:14 +0300

    Add func for find in json file

---
+++ dictionary.py
@@ -0,0 +1,30 @@
+import json
+from difflib import get_close_matches
+
+
+data = json.load(open("dictionary.json"))
+
+
+def get_description(w):
+    w = w.lower()
+    if w in data:
+        return data[w]
+    elif len(get_close_matches(w, data.keys())) > 0:
+        yn = input("Did you mean %s instead? Enter Y if yes, or N if no: " %
+                   get_close_matches(w, data.keys())[0])
+        if yn == "Y":
+            return data[get_close_matches(w, data.keys())[0]]
+        elif yn == "N":
+            return "The word doesn't exist. Please double check it"
+        else:
+            return "We didn't understand your query"
+    else:
+        return "The word doesn't exist. Please double check it"
+
+
+def main():
+    word = input("Enter word: ")
+    print(get_description(word))
+
+if __name__ == "__main__":
+    main()
